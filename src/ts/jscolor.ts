a01cc0d35899500237163487316aa36e980ee288
masonjo <john@e-rehab.com>
2026-03-06 17:45:33 -0800

    dist v2.2.0 (aa992a2)

---
+++ src/ts/jscolor.ts
@@ -0,0 +1,12 @@
+/**
+ * jscolor wrapper for BSEE
+ *
+ * Exposes jscolor library for color picker inputs with data-jscolor attribute.
+ * Usage: <input data-jscolor="{}" value="#ff0000">
+ *
+ * @see https://jscolor.com/
+ */
+
+import jscolor from '@eastdesire/jscolor';
+
+export { jscolor };
